864620a0990f86e7b32dd5cc95006db6ecefa8cf
denizcan.boyraz <denizcan.boyraz@gmail.com>
2019-05-15 18:19:55 +0300

    Add NewRental controller and DTO

---
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using Vidly.DTOs;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDTO newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
